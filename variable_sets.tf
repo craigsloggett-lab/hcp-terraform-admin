559fba27a98e765f0644548f29a068e474215a0c
Craig Sloggett <craigsloggett@gmail.com>
2025-10-16 13:18:12 -0400

    Remove import blocks

--- variable_sets.tf
+++ variable_sets.tf
@@ -1,10 +1,5 @@
 # AWS Provider Credentials
 
-import {
-  id = "varset-oT6SAwv3yoXXptoK"
-  to = tfe_variable_set.aws_provider_authentication
-}
-
 resource "tfe_variable_set" "aws_provider_authentication" {
   name         = var.aws_provider_authentication_variable_set_name
   description  = "The secrets used to authenticate the AWS Provider."
@@ -13,11 +8,6 @@ resource "tfe_variable_set" "aws_provider_authentication" {
 
 # Azure Provider Credentials
 
-import {
-  id = "varset-QG9pFU1edpy7xBZA"
-  to = tfe_variable_set.azurerm_provider_authentication
-}
-
 resource "tfe_variable_set" "azurerm_provider_authentication" {
   name         = var.azurerm_provider_authentication_variable_set_name
   description  = "The secrets used to authenticate the Azure Provider."
@@ -25,11 +15,6 @@ resource "tfe_variable_set" "azurerm_provider_authentication" {
 }
 
 # Microsoft Fabric Provider Credentials
-
-import {
-  id = "varset-4744t5m8b3w7M459"
-  to = tfe_variable_set.fabric_provider_authentication
-}
 
 resource "tfe_variable_set" "fabric_provider_authentication" {
   name         = var.fabric_provider_authentication_variable_set_name
