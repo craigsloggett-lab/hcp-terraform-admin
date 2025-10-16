bce491e3069013215a70e6a3087d2fd64aee0f1d
Craig Sloggett <craigsloggett@gmail.com>
2025-10-16 16:33:37 -0400

    Add Sentinel policy resources

--- policy_sets.tf
+++ policy_sets.tf
@@ -1,2 +1,15 @@
 # Global Policy Set
 
+resource "tfe_policy_set" "global" {
+  name          = "sentinel-policies-global"
+  description   = "A global set of policies to apply across the whole organization."
+  organization  = data.tfe_organization.this.name
+  kind          = "sentinel"
+  policies_path = "policy-sets/global"
+  global        = true
+
+  vcs_repo {
+    identifier     = "${var.github_organization_name}/${var.sentinel_policy_sets_repository_name}"
+    oauth_token_id = data.tfe_oauth_client.github.oauth_token_id
+  }
+}
