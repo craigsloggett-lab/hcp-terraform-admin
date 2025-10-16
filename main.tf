66fe2ed670d569c5792667c8a24918de82564d4c
Craig Sloggett <craigsloggett@gmail.com>
2025-10-16 14:56:08 -0400

    Add the shared services workspace resources

--- main.tf
+++ main.tf
@@ -1,3 +1,8 @@
 data "tfe_organization" "this" {
   name = "craigsloggett-lab"
 }
+
+data "tfe_oauth_client" "github" {
+  organization     = data.tfe_organization.this.id
+  service_provider = "github"
+}
--- variables.tf
+++ variables.tf
@@ -1,3 +1,15 @@
+variable "terraform_version" {
+  type        = string
+  description = "The version of Terraform to use in all workspaces."
+  default     = "1.13.3"
+}
+
+variable "github_organization_name" {
+  type        = string
+  description = "The name of the GitHub organization used to configure the VCS provider."
+  default     = "craigsloggett-lab"
+}
+
 variable "azurerm_provider_authentication_variable_set_name" {
   type        = string
   description = "The name of the variable set used to authenticate the Azure Provider."
@@ -8,4 +20,10 @@ variable "fabric_provider_authentication_variable_set_name" {
   type        = string
   description = "The name of the variable set used to authenticate the Fabric Provider."
   default     = "Microsoft Fabric Provider Authentication"
+}
+
+variable "data_platform_shared_services_workspace_name" {
+  type        = string
+  description = "The name of the workspace managing the Data Platform shared services infrastructure."
+  default     = "azure-fabric-shared-services"
 }
