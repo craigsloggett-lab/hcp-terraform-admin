00085a6e78d2449aaa9fbf2146523b46e8c57c5f
Craig Sloggett <craigsloggett@gmail.com>
2025-10-16 13:17:21 -0400

    Import existing resources

--- variable_sets.tf
+++ variable_sets.tf
@@ -1,5 +1,10 @@
 # AWS Provider Credentials
 
+import {
+  id = "varset-oT6SAwv3yoXXptoK"
+  to = tfe_variable_set.aws_provider_authentication
+}
+
 resource "tfe_variable_set" "aws_provider_authentication" {
   name         = var.aws_provider_authentication_variable_set_name
   description  = "The secrets used to authenticate the AWS Provider."
@@ -8,7 +13,12 @@ resource "tfe_variable_set" "aws_provider_authentication" {
 
 # Azure Provider Credentials
 
-resource "tfe_variable_set" "azurerm" {
+import {
+  id = "varset-QG9pFU1edpy7xBZA"
+  to = tfe_variable_set.azurerm_provider_authentication
+}
+
+resource "tfe_variable_set" "azurerm_provider_authentication" {
   name         = var.azurerm_provider_authentication_variable_set_name
   description  = "The secrets used to authenticate the Azure Provider."
   organization = data.tfe_organization.this.name
@@ -16,7 +26,12 @@ resource "tfe_variable_set" "azurerm" {
 
 # Microsoft Fabric Provider Credentials
 
-resource "tfe_variable_set" "fabric" {
+import {
+  id = "varset-4744t5m8b3w7M459"
+  to = tfe_variable_set.fabric_provider_authentication
+}
+
+resource "tfe_variable_set" "fabric_provider_authentication" {
   name         = var.fabric_provider_authentication_variable_set_name
   description  = "The secrets used to authenticate the Microsoft Fabric Provider."
   organization = data.tfe_organization.this.name
