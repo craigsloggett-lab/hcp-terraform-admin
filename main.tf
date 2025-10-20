f1ca811399bd275f8a69b9db4fd86dccacdea1b9
Craig Sloggett <craigsloggett@gmail.com>
2025-10-20 12:54:32 -0400

    Add moved block

--- main.tf
+++ main.tf
@@ -6,3 +6,8 @@ data "tfe_oauth_client" "github" {
   organization     = data.tfe_organization.this.name
   service_provider = "github"
 }
+
+moved {
+  from = tfe_workspace.data_platform_shared_services
+  to   = tfe_workspace.data_platform_azure_shared_services
+}
