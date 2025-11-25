d667e272cbf47054372c234f409453b3bbaed07a
Craig Sloggett <craigsloggett@gmail.com>
2025-11-25 15:57:23 -0500

    Update module version to a release (#26)
    * Update module version to a release
    
    * Published upstream module

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
 # The bootstrap module is essentially a data source for all of the identifiers being imported.
 module "bootstrap" {
-  source = "git::https://github.com/craigsloggett-lab/terraform-tfe-bootstrap?ref=remove-resource-blocks"
+  source = "git::https://github.com/craigsloggett-lab/terraform-tfe-bootstrap?ref=v0.11.0"
 }
