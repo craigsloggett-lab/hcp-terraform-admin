44a6539604df142e61e35b6a7be8200ab743215e
Craig Sloggett <craigsloggett@gmail.com>
2025-11-25 15:32:32 -0500

    Remove data sources that are already done in the module

--- organization.tf
+++ organization.tf
@@ -1,16 +1,3 @@
-data "tfe_organizations" "this" {}
-
-data "tfe_organization" "this" {
-  name = data.tfe_organizations.this.names[0]
-
-  lifecycle {
-    precondition {
-      condition     = length(data.tfe_organizations.this.names) == 1
-      error_message = "Expected exactly one TFE organization for this token, but found ${length(data.tfe_organizations.this.names)}."
-    }
-  }
-}
-
 resource "tfe_organization" "this" {
   name  = module.bootstrap.tfe_organization.this.name
   email = module.bootstrap.tfe_organization.this.email
