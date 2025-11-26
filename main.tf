67935e6eb7d7d5fc210ad5e4bffc8a4291d0f961
Craig Sloggett <craigsloggett@gmail.com>
2025-11-26 15:01:13 -0500

    Update the module reference to the PMR

--- main.tf
+++ main.tf
@@ -1,4 +1,5 @@
 # The bootstrap module is essentially a data source for all of the identifiers being imported.
 module "bootstrap" {
-  source = "git::https://github.com/craigsloggett-lab/terraform-tfe-bootstrap?ref=v0.11.0"
+  source  = "app.terraform.io/craigsloggett-lab/bootstrap/tfe"
+  version = "0.11.0"
 }
