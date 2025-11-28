1d379ed5ee760fa14c388303f0a022ed6a30aedc
Craig Sloggett <craigsloggett@gmail.com>
2025-11-28 14:59:40 -0500

    Bump the module version number

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 # The discovery module is essentially a data source for all of the identifiers being imported.
 module "discovery" {
   source  = "app.terraform.io/craigsloggett-lab/discovery/tfe"
-  version = "0.12.3"
+  version = "0.12.4"
 }
