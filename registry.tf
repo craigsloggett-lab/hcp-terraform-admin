1fa703964dae82c4b587f3c678cba8b379e4ce00
Craig Sloggett <craigsloggett@gmail.com>
2026-01-27 14:16:49 -0500

    Remove commented out resource

--- registry.tf
+++ registry.tf
@@ -4,19 +4,3 @@ resource "tfe_registry_provider" "hashicorp" {
   registry_name = "public"
   namespace     = "hashicorp"
 }
-
-#resource "tfe_registry_module" "playground" {
-#  for_each = local.playground_modules
-#
-#  test_config {
-#    tests_enabled = true
-#  }
-#
-#  vcs_repo {
-#    display_identifier = "${var.github_organization_name}/${each.key}"
-#    identifier         = "${var.github_organization_name}/terraform-modules-playground"
-#    branch             = "main"
-#    oauth_token_id     = tfe_oauth_client.github.oauth_token_id
-#    source_directory   = each.key
-#  }
-#}
